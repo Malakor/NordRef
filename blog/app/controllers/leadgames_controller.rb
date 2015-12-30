cdf37c90026dd75b74ab7c78b3f8001612798705
jan <jan@localhost.localdomain>
2015-12-30 14:58:16 +0100

    filled controllers with standard functions

--- blog/app/controllers/leadgames_controller.rb
+++ blog/app/controllers/leadgames_controller.rb
@@ -1,14 +1,40 @@
 class LeadgamesController < ApplicationController
 	def index
-	end
-	def new
-	end
-	def create
-	end
-	def edit
+		@leadgames = Leadgames.all
 	end
 	def show
+		## einzelne Spielansichten sind eher unnuetz
+	end
+	def new
+		@leadgame = Leadgame.new
+	end
+	def edit
+		@leadgame = Leadgame.find(params[:id])
+	end
+	def update
+		@leadgame = Leadgame.find(params[:id])
+
+		if @leadgame.update(leadgame_param)
+			redirect_to @leadgame
+		else
+			render 'edit'
+		end
+	end
+	def create
+		@leadgame = Leadgame.new(leadgame_param)
+
+		if @leadgame.save
+			render plain: params[:leadgame].inspect
+		else
+			render 'new'
+		end
 	end
 	def destroy
+	end
+
+
+  private
+	def leadgame_param
+		params.require(:leadgame).permit(:season_g, :amount_g, :referee_id)
 	end
 end
--- blog/app/controllers/licences_controller.rb
+++ blog/app/controllers/licences_controller.rb
@@ -1,14 +1,40 @@
 class LicencesController < ApplicationController
 	def index
-	end
-	def new
-	end
-	def create
-	end
-	def edit
+		# Hier alle anzeigen oder nur alle der Saison?
+		@licenses = Licences.all
 	end
 	def show
+		# Hier alle Lizenzen des Refs anzeigen
+	end
+	def new
+		@licence = Licence.new
+	end
+	def create
+		@licence = Licence.new(licence_param)
+
+		if @licence.save
+			redirect_to @licence
+		else
+			render 'new'
+		end
+	end
+	def edit
+		@licence = Licence.find(params[:id]
+	end
+	def update
+		@licence = Licence.find(params[:id]
+		
+		if @licence.update(licence_param)
+			redirect_to @licence
+		else
+			render 'edit'
+		end
 	end
 	def destroy
+	end
+
+  private
+	def licence_param
+		params.require(:licence).permit(:licence_no, :season_l, :licence_type, :referee_id)
 	end
 end
